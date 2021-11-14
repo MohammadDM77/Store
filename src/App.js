0dc3ce9e23c6c5cf72d7cc5d18d37ffb3be3a7a5
Mohammad Mohammadi <mohammadmohammadi1521377@gmail.com>
2021-11-14 12:54:47 +0330

    'addingStyledComponents'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 // import classes from "./App.module.scss";
-import "./App.module.scss";
+import GlobalStyle from "./GlobalStyle";
+import Footer from "./layouts/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
