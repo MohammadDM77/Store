b964a1a7e04006b8605f5f048d2eb77e7b64bc6e
Mohammad Mohammadi <mohammadmohammadi1521377@gmail.com>
2021-11-14 12:50:57 +0330

    'addingDataImg'

---
+++ src/data/Services.js
@@ -0,0 +1,64 @@
+const Services = [
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/002482b854434bfb8ba281c046bcb16f.w_80,h_80,r_k.png",
+    text: "پرداخت درمحل",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/44c2f7a5fb6d4272b6f944eeafefa58c.w_80,h_80,r_k.png",
+    text: "ارائه فاکتور دقیق",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/bdefcb8f94f6416f9e3848961c5cf2c4.w_80,h_80,r_k.png",
+    text: "ضمانت بازگشت",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/73c66843c3d14e09a18c4374bb2df7b7.w_80,h_80,r_k.png",
+    text: "بسته بندی مناسب",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/37f0aa55051a404abd9695a50db2b579.w_80,h_80,r_k.png",
+    text: "تخفیف‌های عالی",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/3c7498b3f8374a6faf753b13d6b43ecb.w_80,h_80,r_k.png",
+    text: "تحویل سریع",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/002482b854434bfb8ba281c046bcb16f.w_80,h_80,r_k.png",
+    text: "پرداخت درمحل",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/44c2f7a5fb6d4272b6f944eeafefa58c.w_80,h_80,r_k.png",
+    text: "ارائه فاکتور دقیق",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/bdefcb8f94f6416f9e3848961c5cf2c4.w_80,h_80,r_k.png",
+    text: "ضمانت بازگشت",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/73c66843c3d14e09a18c4374bb2df7b7.w_80,h_80,r_k.png",
+    text: "بسته بندی مناسب",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/37f0aa55051a404abd9695a50db2b579.w_80,h_80,r_k.png",
+    text: "تخفیف‌های عالی",
+  },
+  {
+    imgSrc:
+      "https://theme15.mywebzi.ir/uploads/3c7498b3f8374a6faf753b13d6b43ecb.w_80,h_80,r_k.png",
+    text: "تحویل سریع",
+  },
+];
+
+export default Services;
